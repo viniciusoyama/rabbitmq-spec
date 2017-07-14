7924667ea4a3e8a0ace71cc0ffd24fbd6b2993e8
Vinicius Oyama <vinicius.oyama@gmail.com>
2017-07-14 19:37:58 -0300

    increasing base builder coverage

--- spec/integration/dsl/base_builder_spec.rb
+++ spec/integration/dsl/base_builder_spec.rb
@@ -5,6 +5,7 @@ describe 'Using RabbitMQSpec::DSL::Builder::Base' do
     entity = Class.new do
       attr_accessor :attribute1
       attr_accessor :attribute2
+      attr_accessor :hash_attribute
     end
 
     test_builder = Class.new(RabbitMQSpec::DSL::Builder::Base) do
@@ -12,14 +13,42 @@ describe 'Using RabbitMQSpec::DSL::Builder::Base' do
 
       define_dsl_attribute :attribute1
       define_dsl_attribute :attribute2
+      define_dsl_attribute :hash_attribute
     end
 
     entity = test_builder.build do
       attribute1 'value1'
       attribute2 'value2'
+
+      hash_attribute do
+        option1 1
+        option2 2
+        hash_nested do
+          option3 3
+        end
+      end
     end
 
     expect(entity.attribute1).to eq('value1')
     expect(entity.attribute2).to eq('value2')
+    expect(entity.hash_attribute).to eq({ option1: 1, option2: 2, hash_nested: { option3: 3 } })
+  end
+
+  specify 'It warns when an attribute is not allowed to be called on build' do
+    entity = Class.new do
+      attr_accessor :attribute1
+    end
+
+    test_builder = Class.new(RabbitMQSpec::DSL::Builder::Base) do
+      define_entity_class entity
+
+      define_dsl_attribute :attribute1
+    end
+
+    expect {
+      test_builder.build do
+        no_attribute
+      end
+    }.to raise_error
   end
 end
